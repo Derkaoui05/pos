87f28392ba5ac632b3e5df000e7475d15361c60f
Derkaoui05 <147924188+Derkaoui05@users.noreply.github.com>
2026-05-18 15:39:02 +0100

    add prisma and pwa

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+const nextConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  cacheOnFrontEndNav: true,
+  reloadOnOnline: true,
+})({
+  // any other next config
+    reactCompiler: true,
+});
 
 export default nextConfig;
